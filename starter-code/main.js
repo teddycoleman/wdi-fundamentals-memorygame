5aa731ce95d813b903c3947e67224f9641136b31
Teddy Coleman <teddycoleman@Teddys-MacBook-Air.local>
2016-08-30 11:38:51 -0700

    unit 10 assignment with createboard function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,6 +3,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardOne == cardTwo){
 	alert("You found a match!");
 }
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function () {
+	for (var i = 0; i < 4; i++) {
+	    var newCardElement = document.createElement('div');
+	    newCardElement.className = 'card';
+	    gameBoard.appendChild(newCardElement);
+	}
+}
+
+createBoard();
