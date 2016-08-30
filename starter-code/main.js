e83dec0079a91c92ae89062cdc5e39559b9d116c
Teddy Coleman <teddycoleman@Teddys-MacBook-Air.local>
2016-08-30 16:58:48 -0700

    adding code for exercise 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,13 +9,62 @@ if (cardOne == cardTwo){
 }
 */
 
+var cards = ['queen','queen','king','king']
+var cardsInPlay = []
+
 var gameBoard = document.getElementById('game-board');
 
+var isMatch = function(array){
+	if (array[0] == array[1]){
+		alert('you win!');
+	}
+	else{
+		alert('keep trying!');
+	}
+}
+
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+    var resetCards = document.getElementsByClassName('card');
+    for (var i = 0; i < resetCards.length; i++) {
+    	resetCards[i].innerHTML = '';
+    	console.log(resetCards[i]);
+    }
+  }
+
+}
+
+function flip(){
+	if(this.getAttribute('data-card') == 'king'){
+		this.innerHTML = '<img src="king.jpg" alt="King" />';
+	}
+	else {
+		this.innerHTML = '<img src="queen.jpg" alt="Queen" />';
+	}
+}
+
 var createBoard = function () {
-	for (var i = 0; i < 4; i++) {
-	    var newCardElement = document.createElement('div');
-	    newCardElement.className = 'card';
-	    gameBoard.appendChild(newCardElement);
+	for (var i = 0; i < cards.length; i++) {
+	    var cardElement = document.createElement('div');
+	    cardElement.className = 'card';
+	    cardElement.setAttribute('data-card', cards[i]);
+	    cardElement.addEventListener('click', flip);
+	    cardElement.addEventListener('click', isTwoCards);
+	    gameBoard.appendChild(cardElement);
 	}
 }
 
